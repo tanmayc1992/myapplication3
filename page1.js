00a18bf6375190cd926ab1208f0b269fdb026147
D5_62769_Tanmoy <tanmoyc09@gmail.com>
2022-06-23 07:22:32 +0530

    update

--- page1.js
+++ page1.js
@@ -1,10 +1,13 @@
 function isEven(number){
-    if (number %2==0){
-        console.log(`${number} is even`)
-    }
-    else {
-        console.log(`${number} is odd`)
-    }
+if (number%2==0){
+    console.log(`${number} is even`)
 }
+else{
+    console.log(`${number} is not even`)
+
+}
+}
+
+
 isEven(10)
 isEven(11)
