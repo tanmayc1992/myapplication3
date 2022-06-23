dba7dfa73681e40c17b422a8a05003845bafa8a9
D5_62769_Tanmoy <tanmoyc09@gmail.com>
2022-06-23 06:53:46 +0530

    added isEven

--- page1.js
+++ page1.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+function isEven(number){
+    if (number %2==0){
+        console.log(`${number} is even`)
+    }
+    else {
+        console.log(`${number} is odd`)
+    }
+}
+isEven(10)
+isEven(11)
